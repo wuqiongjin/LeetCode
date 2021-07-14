d9c6a0b08c3a28e15ad18730e97a3c401ce29443
wuqiongjin <1301492906@qq.com>
2021-07-14 23:09:16 +0800

    2刷_自己的思路做的

--- LC/数组/34. 在排序数组中查找元素的第1个和最后1个位置.cpp
+++ LC/数组/34. 在排序数组中查找元素的第1个和最后1个位置.cpp
@@ -1,49 +1,110 @@
+// class Solution {
+// public:
+    // int leftborder(vector<int>& nums, int target)
+    // {
+        // int left = 0, right = nums.size() - 1;
+        // while(left <= right)
+        // {
+            // int mid = ((right - left) >> 1) + left;
+            // if(nums[mid] >= target)
+            // {
+                // right = mid - 1;
+            // }
+            // else
+            // {
+                // left = mid + 1;
+            // }
+        // }
+        // return left;
+    // }
+
+    // int rightborder(vector<int>& nums, int target)
+    // {
+        // int left = 0, right = nums.size() - 1;
+        // while(left <= right)
+        // {
+            // int mid = ((right - left) >> 1) + left;
+            // if(nums[mid] > target)
+            // {
+                // right = mid - 1;
+            // }
+            // else
+            // {
+                // left = mid + 1;
+            // }
+        // }
+        // return left;
+    // }
+    // //[left, right)
+    // //left边界是搜查数据的左边界
+    // //right边界是搜查数据的右边界的下一个位置
+    // vector<int> searchRange(vector<int>& nums, int target) 
+    // {
+        // int leftIndex = leftborder(nums, target);
+        // int rightIndex = rightborder(nums, target) - 1;
+        // if(leftIndex > rightIndex)
+            // return vector<int>{-1, -1};
+        // return vector<int>{leftIndex, rightIndex};
+    // }
+// };
+
+
+
+
+
+
+
+
+
+//2刷
+
+//这里找的区间是[left, right]
 class Solution {
 public:
-    int leftborder(vector<int>& nums, int target)
+    //思路：分开寻找Left左边界和Right右边界
+    int Left(vector<int>& nums, int target)
     {
         int left = 0, right = nums.size() - 1;
         while(left <= right)
         {
             int mid = ((right - left) >> 1) + left;
-            if(nums[mid] >= target)
-            {
-                right = mid - 1;
-            }
-            else
+            if(nums[mid] < target)
             {
                 left = mid + 1;
+            }
+            else if(nums[mid] >= target)    //左边界就是满足target的第一个元素
+            {
+                right = mid - 1;
             }
         }
         return left;
     }
 
-    int rightborder(vector<int>& nums, int target)
+    int Right(vector<int>& nums, int target)
     {
         int left = 0, right = nums.size() - 1;
         while(left <= right)
         {
             int mid = ((right - left) >> 1) + left;
-            if(nums[mid] > target)
-            {
-                right = mid - 1;
-            }
-            else
+            if(nums[mid] <= target)         //右边界就是满足target的最后一个元素
             {
                 left = mid + 1;
             }
+            else if(nums[mid] > target)
+            {
+                right = mid - 1;
+            }
         }
-        return left;
+        return right;
     }
-    //[left, right)
-    //left边界是搜查数据的左边界
-    //right边界是搜查数据的右边界的下一个位置
+
     vector<int> searchRange(vector<int>& nums, int target) 
     {
-        int leftIndex = leftborder(nums, target);
-        int rightIndex = rightborder(nums, target) - 1;
-        if(leftIndex > rightIndex)
+        int left = Left(nums, target);
+        int right = Right(nums, target);
+        //判断下表的有效性
+        if(left > right)
             return vector<int>{-1, -1};
-        return vector<int>{leftIndex, rightIndex};
+        return vector<int>{left, right};
     }
 };
